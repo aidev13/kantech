a600d786e6f4ac93358660f7695eea0a6ee8c00b
David Liebherr <ambition.aidev@gmail.com>
2024-04-21 11:27:48 -0500

    WIP, css and input to d8 logic in js and modules

--- kantech_code/components/d8outputs.js
+++ kantech_code/components/d8outputs.js
@@ -1,13 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-const out1 = document.getElementById('out1')
-const out2 = document.getElementById('out2')
-const out3 = document.getElementById('out3')
-const out4 = document.getElementById('out4')
-const out5 = document.getElementById('out5')
-const out6 = document.getElementById('out6')
-const out7 = document.getElementById('out7')
-const out8 = document.getElementById('out8')
+const out1 = document.getElementById("out1")
+const out2 = document.getElementById("out2")
+const out3 = document.getElementById("out3")
+const out4 = document.getElementById("out4")
+const out5 = document.getElementById("out5")
+const out6 = document.getElementById("out6")
+const out7 = document.getElementById("out7")
+const out8 = document.getElementById("out8")
+const inputDoor1 = document.querySelector(".inputGrid")
 
+const out1Positive = document.getElementById('positive')
+const out1Negative = document.getElementById('negative')
 
+console.log(out1Positive)
 
-});
+export function d8Interface() {
+    //   === Add Classlist ===
+    inputDoor1.addEventListener("mouseover", (e) => {
+        if (e.target.id === "input_door1_rex1") {
+            out1.classList.add("_12vled")
+            out1Positive.classList.add("positiveLead")
+            out1Negative.classList.add("negativeLead")
+            
+         }
+      })
+      
+      //   === Remove Classlist ===
+      inputDoor1.addEventListener("mouseout", () => {
+         out1.classList.remove("_12vled")
+         out1Positive.classList.remove("positiveLead")
+         out1Negative.classList.remove("negativeLead")
+    })
+}
--- kantech_code/index.js
+++ kantech_code/index.js
@@ -1,9 +1,11 @@
 import { inputDoor1, inputDoor2 } from "./components/input.js";
 import { readerDoor1, readerDoor2 } from "./components/reader.js";
+import { d8Interface } from "./components/d8outputs.js";
 
 document.addEventListener("DOMContentLoaded", function() {
  inputDoor1();
  inputDoor2();
  readerDoor1();
  readerDoor2();
+ d8Interface();
 });
