19bf8253bd928903b581cb2fe46a6b51a3542d31
David Liebherr <ambition.aidev@gmail.com>
2024-04-21 09:09:47 -0500

    WIP, added d8 board layout

---
+++ kantech_code/components/d8outputs.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+const out1 = document.getElementById('out1')
+const out2 = document.getElementById('out2')
+const out3 = document.getElementById('out3')
+const out4 = document.getElementById('out4')
+const out5 = document.getElementById('out5')
+const out6 = document.getElementById('out6')
+const out7 = document.getElementById('out7')
+const out8 = document.getElementById('out8')
+
+
+
+});
